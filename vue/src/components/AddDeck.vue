bfd72a962418b2a881b6cba4b3b64caab22faf4c
Sam <Samuel.Gassman@gmail.com>
2022-04-11 13:18:03 -0400

    pull

---
+++ vue/src/components/AddDeck.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="add-deck">
+        <label for="deck-name">Deck Name</label>
+        <input type="text" name="deck-name" id="deck-name" v-model="deckName">
+        <button type="submit" v-on:submit="addDeck()">Submit</button>
+    </div>
+</template>
+
+<script>
+import deckService from "../services/DeckService";
+
+export default{
+    data(){
+        return{
+            deckName: "",
+        }
+    },
+    methods: {
+        addDeck(){
+            deckService.addDeck(this.$store.state.user.userId, this.deckName);
+        }
+    }
+}
+</script>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -5,11 +5,17 @@
     <p>
       In the initial configuration, this page can only be seen when logged in.
     </p>
+    <add-deck />
   </div>
 </template>
 
 <script>
+import AddDeck from "../components/AddDeck.vue"
+
 export default {
   name: "home",
+  components: {
+    AddDeck,
+  }
 };
 </script>
