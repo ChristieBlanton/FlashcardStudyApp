859e0844d81bc6eebaa4d2166a460b7f435738d7
ChristieBlanton <94931388+ChristieBlanton@users.noreply.github.com>
2022-04-15 15:19:17 -0400

    work on styling

--- vue/src/components/EditDeck.vue
+++ vue/src/components/EditDeck.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="edit-deck">
-        
+        <h2>Edit Deck</h2>
         <div class="edit-deck-form">
             <label for="deck-name">Deck Name: </label>
             <input type="text" name="deck-name" id="deck-name" v-model="deck.deckName">
             <label for="deck-description">Deck Description: </label>
             <textarea rows="5" cols="40" name="deck-description" id="deck-description" v-model="deck.deckDescription"></textarea>
             <div class="form-btn">
-                <button class="teal-btn" v-on:click="editDeck()">Submit</button>
-                <button class="teal-btn" v-on:click="$router.push({name: 'mydecks'})">Cancel</button>
+                <button class="small-navy-btn" v-on:click="editDeck()">Submit</button>
+                <button class="small-lt-btn" v-on:click="$router.push({name: 'mydecks'})">Cancel</button>
 
             </div>
         </div>
@@ -47,17 +47,26 @@ export default{
 </script>
 
 <style scoped>
+h2 {
+    font-size: 50px;
+    font-weight: bold;
+    font-family: 'Calendas Plus';
+    text-align: center;
+    color: #0D3F67;
+}
 .new-deck-btn{
     height: 40px;
     width: 200px;
 }
 .edit-deck{
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
 }
 .edit-deck-form{
     display: flex;
     flex-direction: column;
+    margin: 60%;
+    margin: 100px;
 }
 #deck-name{
     margin-bottom: 5px;
